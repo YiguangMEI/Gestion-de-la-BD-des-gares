9db1ee9c73d10f493e5a4016096ff5a8b3270c34
jeunecrayon <yiguang.mei@etu.utc.fr>
2023-05-09 22:30:26 +0200

    1

---
+++ rendu3/create.sql
@@ -0,0 +1,274 @@
+/* --------
+
+Supprime les tables, vues et types
+
+-------- */
+
+drop view if exists VvoyageurRegulier CASCADE;
+
+drop view if exists VPrixBillet CASCADE;
+
+drop view if exists VvoyageurOccasionnel CASCADE;
+
+drop table if exists LigneDessert CASCADE;
+
+drop table if exists VoyageDessert CASCADE;
+
+drop table if exists HotelProcheDeGare CASCADE;
+
+drop table if exists TransportProcheDeGare CASCADE;
+
+drop table if exists ExceptionPlannification CASCADE;
+
+drop table if exists VoyagePlannifie CASCADE;
+
+drop table if exists Trajet CASCADE;
+
+drop table if exists Billet CASCADE;
+
+drop table if exists TypePaiment CASCADE;
+
+drop table if exists Voyageur CASCADE;
+
+drop table if exists StatutCarte CASCADE;
+
+drop table if exists JourException CASCADE;
+
+drop table if exists Plannification CASCADE;
+
+drop table if exists Voyage CASCADE;
+
+drop table if exists Train CASCADE;
+
+drop table if exists Ligne CASCADE;
+
+drop table if exists TypeTrain CASCADE;
+
+drop table if exists Hotel CASCADE;
+
+drop table if exists Transport CASCADE ;
+
+drop table if exists Gare CASCADE;
+
+drop type if exists TypeTransport CASCADE;
+
+
+/* Creation des types */
+
+create type TypeTransport as enum ('Bus', 'Metro', 'Tramway', 'Taxi', 'VTC');
+
+/* Creation des tables */
+
+CREATE TABLE Gare (
+  nom VARCHAR,
+  ville VARCHAR,
+  numeroVoie SMALLINT NOT NULL,
+  nomRue VARCHAR NOT NULL,
+  codePostal CHAR(5) NOT NULL,
+  zoneHorraire SMALLINT NOT NULL CHECK (zoneHorraire >= -12 AND zoneHorraire <= 12),
+  PRIMARY KEY (nom, ville)
+);
+
+CREATE TABLE Hotel (
+  id_Hotel INT,
+  nom VARCHAR NOT NULL,
+  numeroVoie SMALLINT NOT NULL,
+  nomRue VARCHAR NOT NULL,
+  codePostal CHAR(5) NOT NULL,
+  ville VARCHAR NOT NULL,
+  PRIMARY KEY (id_Hotel)
+);
+
+CREATE TABLE HotelProcheDeGare (
+    hotel INT REFERENCES Hotel(id_Hotel),
+    nom_gare VARCHAR,
+    ville_gare VARCHAR,
+    FOREIGN KEY (nom_gare, ville_gare) REFERENCES Gare(nom, ville),
+    PRIMARY KEY(hotel, nom_gare, ville_gare)
+);
+
+CREATE TABLE Transport (
+  id_Transpot INT,
+  type TypeTransport NOT NULL,
+  ville VARCHAR NOT NULL,
+  PRIMARY KEY (id_Transpot)
+);
+
+CREATE TABLE TransportProcheDeGare(
+    transport INT REFERENCES Transport(id_Transpot),
+    nom_gare VARCHAR,
+    ville_gare VARCHAR,
+    FOREIGN KEY (nom_gare, ville_gare) REFERENCES Gare(nom, ville),
+    PRIMARY KEY(nom_gare, ville_gare, transport)
+);
+
+CREATE TABLE TypeTrain (
+  nom VARCHAR,
+  nbPlace SMALLINT NOT NULL CHECK (nbPlace >= 0),
+  vitesseMax INT NOT NULL CHECK (vitesseMax >= 0),
+  aPremiereClasse BOOLEAN NOT NULL,
+  coutHeure FLOAT NOT NULL CHECK (coutHeure > 0),
+  PRIMARY KEY (nom)
+);
+
+CREATE TABLE Ligne (
+  numero INT,
+  type_train_autorise VARCHAR NOT NULL,
+  PRIMARY KEY (numero),
+  FOREIGN KEY (type_train_autorise) REFERENCES TypeTrain(nom)
+);
+
+
+CREATE TABLE LigneDessert(
+    nomGare VARCHAR,
+    villeGare VARCHAR,
+    ligne INT,
+    ordre SMALLINT NOT NULL,
+    PRIMARY KEY (nomGare,villeGare,ligne),
+    FOREIGN KEY (nomGare, villeGare) REFERENCES Gare(nom, ville),
+    FOREIGN KEY (ligne) REFERENCES Ligne(numero),
+    UNIQUE (ligne,ordre)
+);
+
+CREATE TABLE Train (
+  numero INT,
+  type VARCHAR NOT NULL,
+  PRIMARY KEY (numero),
+  FOREIGN KEY (type) REFERENCES TypeTrain(nom)
+);
+
+CREATE TABLE Voyage (
+  id_Voyage INTEGER,
+  heureDepart TIME NOT NULL,
+  train INT NOT NULL,
+  ligne INT NOT NULL,
+  PRIMARY KEY (id_Voyage),
+  FOREIGN KEY (train) REFERENCES Train(numero),
+  FOREIGN KEY (ligne) REFERENCES Ligne(numero)
+);
+
+
+CREATE TABLE VoyageDessert(
+    nomGare VARCHAR,
+    villeGare VARCHAR,
+    voyage INT,
+    heureArrivee TIME, -- sera NULL si est depart
+    heureDepart TIME, -- sera NULL si est terminus
+    PRIMARY KEY (nomGare,villeGare,voyage),
+    FOREIGN KEY (nomGare, villeGare) REFERENCES Gare(nom, ville),
+    FOREIGN KEY (voyage) REFERENCES Voyage(id_Voyage)
+);
+
+CREATE TABLE Plannification (
+  idPlanification INT,
+  dateDepart DATE NOT NULL,
+  dateFin DATE NOT NULL,
+  lundi BOOLEAN NOT NULL,
+  mardi BOOLEAN NOT NULL,
+  mercredi BOOLEAN NOT NULL,
+  jeudi BOOLEAN NOT NULL,
+  vendredi BOOLEAN NOT NULL,
+  samedi BOOLEAN NOT NULL,
+  dimanche BOOLEAN NOT NULL,
+  PRIMARY KEY (idPlanification),
+  CHECK (dateDepart < dateFin),
+  CHECK (lundi or mardi or mercredi or jeudi or vendredi or samedi or dimanche)
+);
+
+
+CREATE TABLE JourException (
+  jour DATE,
+  ajout BOOLEAN NOT NULL,
+  PRIMARY KEY (jour)
+);
+
+CREATE TABLE ExceptionPlannification(
+    jour DATE REFERENCES JourException(jour),
+    plannification INT REFERENCES Plannification(idPlanification),
+    PRIMARY KEY(jour, plannification)
+
+);
+
+CREATE TABLE VoyagePlannifie(
+    plannification INT REFERENCES Plannification(idPlanification),
+    voyage INT REFERENCES Voyage(id_Voyage),
+    PRIMARY KEY (plannification, voyage)
+
+);
+
+CREATE TABLE StatutCarte (
+  intitule VARCHAR,
+  PRIMARY KEY (intitule)
+);
+
+CREATE TABLE Voyageur (
+  idVoyageur INTEGER,
+  nom VARCHAR NOT NULL,
+  prenom VARCHAR NOT NULL,
+  numeroVoie SMALLINT NOT NULL,
+  nomRue VARCHAR NOT NULL,
+  codePostal CHAR(5) NOT NULL,
+  tel VARCHAR UNIQUE NOT NULL,
+  numeroCarte INT UNIQUE,
+  statut VARCHAR,
+  PRIMARY KEY (idVoyageur),
+  FOREIGN KEY (statut) REFERENCES StatutCarte(intitule),
+  CHECK ((numeroCarte IS NULL AND statut IS NULL) OR (numeroCarte IS NOT NULL AND statut IS NOT NULL))
+);
+
+CREATE TABLE TypePaiment (
+  intitule VARCHAR,
+  PRIMARY KEY (intitule)
+);
+
+CREATE TABLE Billet (
+  idBillet INTEGER PRIMARY KEY,
+  assurance BOOLEAN NOT NULL,
+  acheteur INTEGER NOT NULL,
+  typePaiement VARCHAR,
+  FOREIGN KEY (acheteur) REFERENCES Voyageur(idVoyageur),
+  FOREIGN KEY (typePaiement) REFERENCES TypePaiment(intitule)
+);
+
+CREATE TABLE Trajet (
+  billet INTEGER,
+  numeroTrajet INTEGER,
+  numeroPlace SMALLINT NOT NULL,
+  voyage INT NOT NULL,
+  prix DECIMAL(5,2) NOT NULL,
+  heureDepart TIME NOT NULL,
+  heureArrivee TIME NOT NULL, 
+  date DATE NOT NULL, --A tester
+  PRIMARY KEY (billet, numeroTrajet),
+  CHECK (numeroPlace >= 0 AND numeroTrajet >= 0),
+  FOREIGN KEY (billet) REFERENCES Billet(idBillet),
+  FOREIGN KEY (voyage) REFERENCES Voyage(id_Voyage)
+);
+
+
+/* ------------
+
+Creer les vues
+
+------------ */
+
+-- Vue VvoyageurRégulier
+CREATE VIEW VvoyageurRegulier AS 
+SELECT idVoyageur, nom, prenom, numeroVoie, nomRue, codePostal, tel, numeroCarte, statut 
+FROM Voyageur 
+WHERE statut IS NOT NULL;
+
+-- Vue VvoyageurOccasionnel
+CREATE VIEW VvoyageurOccasionnel AS 
+SELECT idVoyageur, nom, prenom, numeroVoie, nomRue, codePostal, tel 
+FROM Voyageur 
+WHERE statut IS NULL;
+
+-- Vue PrixBillet
+CREATE VIEW VPrixBillet AS 
+SELECT idBillet, (SELECT SUM(prix) as prixBillet 
+  FROM trajet 
+  WHERE trajet.billet=idBillet
+) 
+from billet;
+
---
+++ rendu3/requete.sql
@@ -0,0 +1,129 @@
+/* ------------
+
+les requêtes sympa à faire
+ 
+------------ */
+
+--les numeros des lignes desservant Paris : simple
+SELECT l.numero 
+FROM ligne l INNER JOIN lignedessert ld ON l.numero = ld.ligne
+WHERE UPPER(ld.villeGare)='PARIS';
+
+--les voyages disponibles pour faire Paris-Compiègne ainsi que les horraires correspondantes (sans tenir compte des planifications)
+SELECT vg.voyage,vg.heureDepart,vd.heurearrivee
+FROM voyageDessert vg INNER JOIN voyageDessert vd ON vg.voyage = vd.voyage
+WHERE vg.villeGare='Paris' AND vd.villeGare='Compiegne';
+
+--Les jours où ce voyage Paris-Compiègne est disponible d'après la planification (sans tenir compte des exceptions)
+SELECT dateDepart,dateFin,lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche 
+FROM plannification p INNER JOIN VoyagePlannifie vp ON p.idPlanification=vp.plannification
+WHERE vp.voyage=(SELECT vg.voyage 
+    FROM voyageDessert vg INNER JOIN voyageDessert vd ON vg.voyage=vd.voyage
+    WHERE vg.villeGare='Paris' AND vd.villeGare='Compiegne'
+);
+
+-- Les voyages Paris-Compiègne annulés à cause d'une exception
+
+SELECT je.jour, 'voyage annulé ce jour-là' as indication 
+FROM plannification p
+INNER JOIN VoyagePlannifie vp  ON p.idPlanification=vp.plannification
+INNER JOIN ExceptionPlannification ep  ON ep.plannification=p.idPlanification
+INNER JOIN JourException je ON ep.jour =je.jour
+WHERE vp.voyage=(SELECT vg.voyage 
+    FROM voyageDessert vg INNER JOIN voyageDessert vd ON vg.voyage=vd.voyage
+    WHERE vg.villeGare='Paris' AND vd.villeGare='Compiegne'
+) AND (je.ajout=FALSE);
+
+-- Les voyages Paris-Compiègne ajouté à cause d'une exception
+
+SELECT je.jour, 'voyage ajouté jour-là' as indication 
+FROM plannification p
+INNER JOIN VoyagePlannifie vp  ON p.idPlanification=vp.plannification
+INNER JOIN ExceptionPlannification ep  ON ep.plannification=p.idPlanification
+INNER JOIN JourException je ON ep.jour = je.jour
+WHERE vp.voyage=(SELECT vg.voyage 
+    FROM voyageDessert vg INNER JOIN voyageDessert vd ON vg.voyage=vd.voyage
+    WHERE vg.villeGare='Paris' AND vd.villeGare='Compiegne'
+) AND (je.ajout=TRUE);
+
+-- Un utilisateur veut savoir quels voyages sont disponibles pour faire Paris-Compiègne le dimanche 05\03\2023
+SELECT vg.voyage,vg.heureDepart,vd.heurearrivee
+FROM voyageDessert vg
+INNER JOIN voyageDessert vd ON vg.voyage=vd.voyage
+INNER JOIN VoyagePlannifie vp ON vg.voyage=vp.voyage
+INNER JOIN Plannification p ON vp.plannification=p.idPlanification
+INNER JOIN ExceptionPlannification ep  ON ep.plannification=p.idPlanification
+INNER JOIN JourException je ON ep.jour =je.jour
+WHERE vg.villeGare='Paris' AND vd.villeGare='Compiegne' AND p.dateDepart<='2023-03-05' AND p.dateFin>='2023-03-05' AND (p.dimanche=TRUE OR (je.jour='2023-03-05' and je.ajout=TRUE)) AND NOT (je.jour='2023-03-05' and je.ajout=FALSE);
+
+--prix moyen d'un billet acheté
+SELECT AVG(PB.prixBillet)
+FROM VPrixBillet PB;
+
+--prix moyen d'un billet selon le moyen de paiement
+SELECT B.typePaiement, AVG(PB.prixBillet)
+FROM VPrixBillet PB
+INNER JOIN Billet B ON
+PB.idBillet=B.idBillet
+GROUP BY(B.typePaiement);
+
+
+--prix moyen d'un billet payé par statut
+SELECT v.statut, AVG(PB.prixBillet)
+FROM VPrixBillet PB
+INNER JOIN Billet B ON
+PB.idBillet=B.idBillet
+INNER JOIN voyageur v ON
+B.acheteur=v.idvoyageur
+WHERE statut IS NOT NULL
+GROUP BY (v.statut);
+
+-- le nombre de trajet fait par des voyageur pour un voyage donné ici =1 à une date donné ici='2023-05-03' et une heure de départ ici = '08:00:00'
+SELECT count(*) as nombre_place_occupe FROM trajet t
+WHERE t.voyage=1 and t.date='2023-05-03' AND t.heureDepart= '08:00:00';
+
+-- le nombre de place par voyage
+SELECT v.id_voyage, tp.nbPlace FROM voyage v
+INNER JOIN ligne l ON v.ligne=l.numero
+INNER JOIN TypeTrain tp ON l.type_train_autorise=tp.nom;
+
+-- le nombre de place pour un voyage donné ici =1
+SELECT tp.nbPlace FROM voyage v
+INNER JOIN ligne l ON v.ligne=l.numero
+INNER JOIN TypeTrain tp ON l.type_train_autorise=tp.nom
+WHERE v.id_voyage=1;
+
+
+-- Le nombre de place restante pour un voyage donné ici =1 à une date donné ici='2023-05-03' et une heure de départ ici = '08:00:00'
+
+SELECT count(*) as nombre_place_occupe,
+ tp.nbPlace as nombre_place_disponible,
+ tp.nbPlace-count(*) as place_disponible
+FROM trajet t
+INNER JOIN voyage v ON t.voyage=v.id_voyage
+INNER JOIN ligne l ON v.ligne=l.numero
+INNER JOIN TypeTrain tp ON l.type_train_autorise=tp.nom
+WHERE t.voyage=1 and t.date='2023-05-03' AND v.heureDepart= '08:00:00'
+GROUP BY (tp.nbPlace);
+
+-- Les voyages d'un voyageur
+
+SELECT voyage.heureDepart, voyage.ligne,voyage.train
+From voyage
+JOIN Trajet T on
+T.voyage = voyage.id_voyage
+JOIN Billet B on
+B.idBillet = T.billet
+Where B.acheteur ='1 ';   -- id de voyageur
+
+-- Compte le nombre de trajet de un voyageur
+Select count(*)
+From Trajet T
+JOIN Billet B on
+T.billet = B.idBillet
+Where B.acheteur='1' ; --id de voyageur
+
+-- Compte le nombre de hotels  proche des gare (nombre de transport)
+Select count(*)
+From HotelProcheDeGare H
+GROUP BY(H.nom_gare);
